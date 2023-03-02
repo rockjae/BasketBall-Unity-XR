683f94d1f9b036539f0cf3a2a8f534e2dabed53a
Ayush Gupta <ayushgupta6719@gmail.com>
2023-03-02 23:21:26 +0530

    AI - P1 animation

--- Assets/02_Scripts/AiController.cs
+++ Assets/02_Scripts/AiController.cs
@@ -6,17 +6,43 @@ using UnityEngine.AI;
 public class AiController : MonoBehaviour
 {
     public NavMeshAgent agent;
-
+    public Animator animator;
     public GameObject player;
+
+    private bool shouldFollow = false;
+    private bool ret = false;
+
+    private Transform startPos;
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 180,0);
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        startPos = agent.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.transform.position);
+        if (shouldFollow)
+        {
+            agent.SetDestination(player.transform.position);
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+        }
+        else if (ret)
+        {
+            agent.SetDestination(player.transform.position);
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        shouldFollow = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        ret = true;
     }
 }
